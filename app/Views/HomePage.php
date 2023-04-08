0a3f2e9497764aca114f2125d9c7d5e293813a67
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-04-08 14:48:27 -0300

    sistema de layout com renderSection

--- app/Views/HomePage.php
+++ app/Views/HomePage.php
@@ -1,69 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>SideChest'o - Acompanhe sua evolução corporal</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">SideChest'o</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Início</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Sobre</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contato</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <header class="jumbotron jumbotron-fluid">
-      <div class="container">
+<?php $this->extend('layout') ?>
+<?php $this->section('content') ?>
+<header class="jumbotron jumbotron-fluid">
+    <div class="container">
         <h1 class="display-4">Acompanhe sua evolução corporal com SideChest'o</h1>
         <p class="lead">Tenha acesso ao seu cálculo de IMC, perda de gordura, mudança de medidas corporais e fichas de treino juntamente de suas evoluções.</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Comece agora</a>
-      </div>
-    </header>
-    <section class="container">
-      <div class="row">
-        <div class="col-md-4">
-          <h2>IMC</h2>
-          <p>Calcule seu índice de massa corporal e acompanhe sua evolução.</p>
-          <a class="btn btn-secondary" href="#" role="button">Saiba mais</a>
-        </div>
-        <div class="col-md-4">
-          <h2>Perda de gordura</h2>
-          <p>Acompanhe sua evolução na perda de gordura e saiba se está no caminho certo.</p>
-          <a class="btn btn-secondary" href="#" role="button">Saiba mais</a>
-        </div>
-        <div class="col-md-4">
-          <h2>Mudança de medidas</h2>
-          <p>Veja como suas medidas corporais mudam ao longo do tempo.</p>
-          <a class="btn btn-secondary" href="#" role="button">Saiba mais</a>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <h2>Fichas de treino</h2>
-          <p> Tenha acesso a fichas de treino personalizadas e acompanhe sua evolução em cada exercício.</p>
-      <a class="btn btn-secondary" href="#" role="button">Saiba mais</a>
     </div>
-  </div>
+</header>
+<section class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <h2>IMC</h2>
+            <p>Calcule seu índice de massa corporal e acompanhe sua evolução.</p>
+            <a class="btn btn-secondary" href="#" role="button">Saiba mais</a>
+        </div>
+        <div class="col-md-4">
+            <h2>Perda de gordura</h2>
+            <p>Acompanhe sua evolução na perda de gordura e saiba se está no caminho certo.</p>
+            <a class="btn btn-secondary" href="#" role="button">Saiba mais</a>
+        </div>
+        <div class="col-md-4">
+            <h2>Mudança de medidas</h2>
+            <p>Veja como suas medidas corporais mudam ao longo do tempo.</p>
+            <a class="btn btn-secondary" href="#" role="button">Saiba mais</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Fichas de treino</h2>
+            <p> Tenha acesso a fichas de treino personalizadas e acompanhe sua evolução em cada exercício.</p>
+            <a class="btn btn-secondary" href="#" role="button">Saiba mais</a>
+        </div>
+    </div>
 </section>
-<footer class="bg-light py-3">
-  <div class="container">
-    <p class="text-center">Copyright © 2023 SideChest'o</p>
-  </div>
-</footer>
-  </body>
-</html>
+<?php $this->endSection() ?>
---
+++ app/Views/sobre.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <h1 class="my-4">SideChest'o</h1>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link" href="index.html">Início</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Sobre</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="analise.html">Análise Corporal</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="treinos.html">Fichas de Treino</a>
+        </li>
+    </ul>
+    <div class="tab-content">
+        <div id="sobre" class="tab-pane fade show active">
+            <h2 class="mt-4">Sobre o SideChest'o</h2>
+            <p>O SideChest'o é um sistema online desenvolvido para auxiliar atletas e praticantes de atividades físicas a monitorarem sua evolução esportiva e corpórea de forma simples e prática.</p>
+            <p>Com o SideChest'o, é possível:</p>
+            <ul>
+                <li>Calcular o seu Índice de Massa Corporal (IMC) e monitorar suas mudanças ao longo do tempo;</li>
+                <li>Registrar suas fichas de treino e acompanhar sua evolução nos exercícios;</li>
+                <li>Acessar gráficos e relatórios sobre seus resultados e progresso;</li>
+                <li>Ter todas as informações em um só lugar, de forma segura e organizada.</li>
+            </ul>
+            <p>O SideChest'o foi desenvolvido por uma equipe de especialistas em esportes e tecnologia, visando atender às necessidades dos atletas modernos que buscam acompanhar sua evolução de forma mais eficiente.</p>
+            <p>Experimente o SideChest'o agora mesmo e tenha todo o seu progresso esportivo e corpóreo em um só lugar!</p>
+        </div>
+    </div>
+</div>
