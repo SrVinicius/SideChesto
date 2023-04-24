c079f79d568767d08a58dad34f513eba999abe58
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-04-23 22:15:12 -0300

    DataTable Treinos

--- app/Controllers/Ajax/Treinos.php
+++ app/Controllers/Ajax/Treinos.php
@@ -1,10 +1,18 @@
 <?php namespace App\Controllers\Ajax;
 
+use App\Models\TreinoModel;
 use CodeIgniter\Controller;
 
 Class Treinos extends Controller {
     
     public function getDados(){
 
+        $treinosModel = new TreinoModel();
+
+        echo '<pre>';
+
+        $treinos = $treinosModel->findAll();
+
+        print_r($treinos);
     }
 } 
---
+++ app/Models/TreinosModel.php
@@ -0,0 +1,8 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TreinoModel extends Model{
+    protected $table = 'treinos';
+    protected $primaryKey = 'id';
+}
