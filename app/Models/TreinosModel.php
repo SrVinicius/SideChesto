b1630b76638899195d9fec9a30d439e42bf06ea9
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-05-07 14:05:41 -0300

    Treino por cliente

--- app/Models/TreinosModel.php
+++ app/Models/TreinosModel.php
@@ -5,4 +5,16 @@ use CodeIgniter\Model;
 class TreinoModel extends Model{
     protected $table = 'treinos';
     protected $primaryKey = 'id';
+
+    function TreinoPorCliente ($id){
+        return db_connect()->query("
+            SELECT 
+                TIPO_TREINO 
+            FROM 
+                treinos
+            WHERE 
+                cliente_id = $id
+        ");
+    }
+
 }
