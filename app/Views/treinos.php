39fce730f9d0cc4c15c750d8c8f74670e7beef0f
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-05-20 14:48:08 -0300

    tabela de treinos

--- app/Views/treinos.php
+++ app/Views/treinos.php
@@ -1,25 +1,24 @@
-<?php $this->extend('layout') ?>
-<?php $this->section('content') ?>
+<?php $this->extend('layout'); ?>
+<?php $this->section('content'); ?>
 
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         $('#tableTreinos').DataTable({
             "ajax": "data/arrays.txt"
         });
     });
 </script>
 
+<?php
+$db = db_connect();
+$sql = "SELECT * FROM TREINOS";
+$result = $db->query($sql);
+?>
+
 <div class="container">
     <div class="tab-content">
         <div id="sobre" class="tab-pane fade show active">
             <h2 class="mt-4">Ficha de treinos</h2>
-            // selecionar todos os treinos
-            <?php 
-                $db = db_connect();
-                $sql = "SELECT * FROM TREINOS"; 
-                $result = $db->query($sql);
-                var_dump($result->getResultObject());
-            ?>
         </div>
     </div>
     <table class="table" id="tableTreinos">
@@ -29,6 +28,14 @@
                 <th>ID do cliente</th>
             </tr>
         </thead>
+        <tbody>
+            <?php foreach ($result->getResultArray() as $treino): ?>
+                <tr>
+                    <td><?php echo $treino['tipo_treino']; ?></td>
+                    <td><?php echo $treino['cliente_id']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
     </table>
 </div>
-<?php $this->endSection() ?> 
+<?php $this->endSection(); ?>
