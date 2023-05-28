9905d902fedf7f53840fa48eae6189287f3aef6f
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-05-27 22:49:28 -0300

    27/05

--- app/Views/cadastrotreino.php
+++ app/Views/cadastrotreino.php
@@ -1,1 +1,6 @@
-area de cadastro de treino (upload de arquivo).
+<?php $this->extend('layout'); ?>
+<?php $this->section('content'); ?>
+
+<h1>area de cadastro de treino (upload de arquivo).</h1>
+
+<?php $this->endSection(); ?>
