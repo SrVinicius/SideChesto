9a43a832746273f4af0797c6a0cf1a02ff13e8a3
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-05-28 15:09:47 -0300

    28/05-v.02

--- app/Views/cadastrotreino.php
+++ app/Views/cadastrotreino.php
@@ -1,13 +1,20 @@
 <?php $this->extend('layout'); ?>
 <?php $this->section('content'); ?>
 
-<h1>Adicione o treino aqui</h1>
-
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <label for="pdfFile">Selecione um arquivo em PDF:</label>
-        <input type="file" name="pdfFile" id="pdfFile" accept=".pdf">
-        <br><br>
-        <input type="submit" value="Enviar">
-    </form>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h1>Adicione os treinos aqui</h1>
+        </div>
+        <div class="card-body">
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+                <label for="File">Selecione um arquivo em PDF:</label>
+                <input type="file" name="pdfFile" id="File" accept=".pdf">
+                <br><br>
+                <input type="submit" value="Enviar" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
+</div>
 
 <?php $this->endSection(); ?>
