ce3d7d33691bda33dccf1c439cff50d24dc1374a
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-05-28 15:06:45 -0300

    28/05

--- app/Views/cadastrotreino.php
+++ app/Views/cadastrotreino.php
@@ -1,6 +1,13 @@
 <?php $this->extend('layout'); ?>
 <?php $this->section('content'); ?>
 
-<h1>area de cadastro de treino (upload de arquivo).</h1>
+<h1>Adicione o treino aqui</h1>
+
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <label for="pdfFile">Selecione um arquivo em PDF:</label>
+        <input type="file" name="pdfFile" id="pdfFile" accept=".pdf">
+        <br><br>
+        <input type="submit" value="Enviar">
+    </form>
 
 <?php $this->endSection(); ?>
