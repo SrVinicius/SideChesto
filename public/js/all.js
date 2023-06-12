ab785564a718789b1b097bb13896acab5eca7932
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-06-11 22:36:40 -0300

    11/06

---
+++ public/js/all.js
@@ -0,0 +1,40 @@
+$("form").submit(function (e) {
+    e.preventDefault();
+    var form = $(this);
+    var nome = $(this).attr("name")
+    var id = $(this).attr("id")
+    var validation = $(this).attr("validation")
+    if (validation == 1) {
+        $.ajax({
+            type: "POST",
+            url: form.attr('action'),
+            enctype: 'multipart/form-data',
+            //data: $(this).serialize(),
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData: false,
+            beforeSend: function () {
+                Carregamento();
+            },
+            success: function (data) {
+                // add logica
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: jqXHR['responseJSON']['message'],
+                    confirmButtonColor: "#f52525",
+                });
+            }
+        }).fail(function (jqXHR, textStatus, msg) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: jqXHR['responseJSON']['message'],
+                confirmButtonColor: "#f52525",
+            });
+        });
+    }
+});
