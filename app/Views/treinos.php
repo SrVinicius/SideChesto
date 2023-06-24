cfe683bacab45388cb47c730f2a756e3de99ecc1
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-06-24 17:49:18 -0300

    v.01_24/06

--- app/Views/treinos.php
+++ app/Views/treinos.php
@@ -1,14 +1,5 @@
 <?php $this->extend('layout'); ?>
 <?php $this->section('content'); ?>
-
-<script>
-    $(document).ready(function() {
-        $('#tableTreinos').DataTable({
-            "ajax": "data/arrays.txt"
-        });
-    });
-</script>
-
 <div class="container">
     <div class="tab-content">
         <div id="sobre" class="tab-pane fade show active">
