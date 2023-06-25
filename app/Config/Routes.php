ed08875c5acb8526c7317c2c42065317a67ec9ad
SrVinicius <viniciusferreirarosario5@gmail.com>
2023-06-25 00:10:58 -0300

    v.01_25/06

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,7 @@ $routes->set404Override();
 $routes->get('/', 'Home::index');
 $routes->get('sobre', 'Home::sobre');
 $routes->get('contato', 'Home::contato');
+//$routes->get('treinos', 'Treinos::FindTreinos');
 $routes->match(['get', 'post'], 'treinos', 'Treinos::FindTreinos');
 $routes->get('pesos', 'Home::pesos');
 
